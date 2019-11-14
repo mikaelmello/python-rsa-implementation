093603863e124895eeeb4136dd83899d2ed0e61a
gtaumaturgo <gtaumaturg@gmail.com>
2019-11-14 00:22:43 -0300

    too lazy to explain

---
+++ main.py
@@ -0,0 +1,68 @@
+import secrets
+import math
+def mrand(l, r):
+    sz = r - l + 1
+    return l + secrets.randbelow(sz)
+
+def mulmod(a,b,c):
+    return (a*b)%c
+
+def fexp(num, exponent, mod):
+    if exponent == 0 :
+        return 1
+    term = fexp(mulmod(num,num,mod), exponent//2, mod)
+    if exponent % 2 == 0:
+        return term
+    else :
+        return mulmod(term,num,mod)
+        
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    s = 0
+    d = n - 1
+    while(d % 2 == 0) :
+        d = d // 2
+        s = s + 1
+    for k in range(64) :
+        a = mrand(2,n)
+        x = fexp(a,d,n)
+        if x != 1 and x != n-1 :
+            for r in range(1,s):
+                x = mulmod(x,x,n)
+                if x == 1 :
+                    return False
+                if x == n-1 :
+                    break
+            if x != n-1 :
+                return False
+    return True
+
+def get_primes():
+    n = mrand(22,30)
+    base_num = 2**n
+    x = base_num
+    while(not is_prime(x)):
+        x = x + 1
+    while(not is_prime(base_num)):
+        base_num = base_num - 1
+    return (x, base_num)
+
+p1, p2 = get_primes()
+N = p1 * p2
+phiN = (p1-1) * (p2-1)
+print(p1,p2)
+print(phiN)
+e = mrand(10, 1000)
+while(math.gcd(e, N) != 1 or math.gcd(e, phiN) != 1) :
+    e = e + 1
+d = fexp(e,phiN-1,phiN)
+d1 = fexp(e,phiN-2,phiN)
+d2 = fexp(e,phiN,phiN)
+print(math.gcd(e, N), math.gcd(e,phiN))
+print(e, d)
+print(mulmod(e, d, phiN))
+print(mulmod(e, d1, phiN))
+print(mulmod(e, d2, phiN))
