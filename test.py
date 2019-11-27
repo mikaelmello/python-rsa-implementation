23093d12837c7ad4b3dfb1a02d5b6910ba30cf03
Mikael Mello <git@mikaelmello.com>
2019-11-27 02:58:38 -0300

    Use library for unit tests
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
+import rsa
+import math_utils
+import unittest
+import sys
+sys.setrecursionlimit(1500)
 
-def extended_euclid(a,b) :
-    if b == 0:
-        return a, 1, 0
-    q, w, e = extended_euclid(b,a % b)
-    return q, e, w - e *(a//b)
 
-print(extended_euclid(5,16))
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.public_key, self.private_key = rsa.generate_key_pair()
+
+    def test_n_equal(self):
+        self.assertEqual(self.public_key.n, self.private_key.n,
+                         "Both keys have different Ns")
+
+    def test_number_to_ed_eq_number(self):
+        for i in range(100):
+            random_number = math_utils.mrand(1, self.public_key.n)
+            result = math_utils.fexp(
+                random_number, self.public_key.exp * self.private_key.exp, self.public_key.n)
+            self.assertEqual(random_number, result,
+                             "Number to e*d is not equal to itself mod n")
+
+
+if __name__ == '__main__':
+    unittest.main()
