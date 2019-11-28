76dc675d7d0d2f1e8a638e42dca23e4ce7fa4750
Mikael Mello <git@mikaelmello.com>
2019-11-28 17:19:04 -0300

    Add import and export of keys
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- rsa.py
+++ rsa.py
@@ -1,15 +1,85 @@
 from math_utils import *
+import asn1tools
+import base64
 
 
 class PublicKey:
+    def export(self, output_file=''):
+        with open(output_file, 'w+') as theFile:
+            encoder = asn1tools._compile_files(
+                './asn1-structures/RSAPublicKey.asn', 'der', 'der', '/tmp')
+            a = encoder[0].encode('RSAPublicKey', {
+                'modulus': self.n, 'publicExponent': self.e})
+            b = base64.b64encode(a).decode('ascii')
+            limit = 64
+            offset = 0
+            key = f'-----BEGIN PUBLIC KEY-----\r\n'
+            while offset < len(b):
+                key += b[offset:offset+limit] + '\r\n'
+                offset += limit
+            key += '-----END PUBLIC KEY-----'
+            theFile.write(key)
+
+    @classmethod
+    def fromString(cls, input_file):
+        with open(input_file, 'r') as theFile:
+            data = theFile.readlines()
+            encoded = ''
+            for i in range(1, len(data)-1):
+                encoded += data[i]
+            encoder = asn1tools._compile_files(
+                './asn1-structures/RSAPublicKey.asn', 'der', 'der', '/tmp')
+            content = base64.b64decode(encoded)
+            x = encoder[0].decode('RSAPublicKey', content)
+            return cls(x['publicExponent'], x['modulus'])
+
     def __init__(self, e, n):
         self.n = n
         self.e = e
 
 
 class PrivateKey:
+    def export(self, output_file):
+        with open(output_file, 'w+') as theFile:
+            encoder = asn1tools._compile_files(
+                './asn1-structures/RSAPrivateKey.asn', 'der', 'der', '/tmp')
+            a = encoder[0].encode('RSAPrivateKey', {
+                'modulus': self.n,
+                'publicExponent': self.e,
+                'privateExponent': self.d,
+                'prime1': self.p,
+                'prime2': self.q,
+                'exponent1': self.dp,
+                'exponent2': self.dq,
+                'coefficient': self.qinv,
+                'version': 0,
+            })
+            b = base64.b64encode(a).decode('ascii')
+            limit = 64
+            offset = 0
+            key = f'-----BEGIN PUBLIC KEY-----\r\n'
+            while offset < len(b):
+                key += b[offset:offset+limit] + '\r\n'
+                offset += limit
+            key += '-----END PUBLIC KEY-----'
+            theFile.write(key)
+
+    @classmethod
+    def fromString(cls, output_file):
+        with open(output_file, 'r') as theFile:
+            data = theFile.readlines()
+            encoded = ''
+            for i in range(1, len(data)-1):
+                encoded += data[i]
+            encoder = asn1tools._compile_files(
+                './asn1-structures/RSAPrivateKey.asn', 'der', 'der', '/tmp')
+            content = base64.b64decode(encoded)
+            x = encoder[0].decode('RSAPrivateKey', content)
+            return cls(x['publicExponent'], x['privateExponent'], x['prime1'], x['prime2'])
+
     def __init__(self, e, d, p, q):
         self.n = p*q
+        self.e = e
         self.d = d
         self.p = p
         self.q = q
@@ -19,7 +89,7 @@ class PrivateKey:
 
 
 def generate_key_pair(bit_length):
-    allowed_bit_lengths = [1024, 2048, 3072, 4096]
+    allowed_bit_lengths = [512, 1024, 2048, 3072, 4096]
 
     if bit_length not in allowed_bit_lengths:
         print(f'Key bit length {bit_length} not allowed')
